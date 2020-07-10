4423abdf1e084f4648c77d5b3d6262e69042e29e
Poke <sergamer15@gmail.com>
2020-07-10 05:38:50 +0200

    Im stupid, Fixed

--- build/AlPo_hud/fxmanifest.lua
+++ build/AlPo_hud/fxmanifest.lua
@@ -5,7 +5,7 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
-  'AlPo_hud_cl..net.dll'
+  'AlPo_hud_cl.net.dll'
 }
 
 server_scripts {
